710c269a663f001733647eb8fc3eea59d72db3ff
YJCMS <sen9563@gmail.com>
2024-08-12 13:43:22 +0000

    add coalesce, malloc, findfit, place

--- mm.c
+++ mm.c
@@ -59,7 +59,7 @@ team_t team = {
 
 /* Read the size and allocated field from address p */
 #define GET_SIZE(p) (GET(p) & ~0x7)
-#define GET_SIZE(p) (GET(p) & 0x1)
+#define GET_ALLOC(p) (GET(p) & 0x1)
 
 /* Given block ptr bp, compute address of its header and footer */
 #define HDRP(bp) ((char *)(bp) - WSIZE)
@@ -71,7 +71,7 @@ team_t team = {
 
 static char *heap_listp;
 static void *extend_heap(size_t words);
-
+static void *coalesce(void *bp);
 
 /* 
  * mm_init - initialize the malloc package.
@@ -108,20 +108,79 @@ static void *extend_heap(size_t words) {
 
 }
 
+/* first fit 적용 */
+static void *find_fit(size_t asize) {
+    /* first fit 탐색 */
+    void *bp;
+    
+    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp))&& (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+    return NULL; 
+}
+
+/* place 함수 */
+static void place(void *bp, size_t asize) {
+    size_t csize = GET_SIZE(HDRP(bp)); // 현재 있는 블록 사이즈
+
+    if ((csize - asize) >= (2*DSIZE)) {
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(HDRP(bp), PACK(csize-asize, 0));
+        PUT(FTRP(bp), PACK(csize-asize, 0));
+    }
+
+    else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
+
 /* 
  * mm_malloc - Allocate a block by incrementing the brk pointer.
  *     Always allocate a block whose size is a multiple of the alignment.
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+    size_t asize; // 블록 크기 조정
+    size_t extendsize; // 안맞으면 확장시킬 양
+    char *bp;
+
+    /* 잘못돤 요청 무시 */
+    if (size == 0)
+        return NULL;
+    
+    /* overhead, alignment 요청 포함해서 블록 크기 조정 */
+    if (size<= DSIZE)
+        asize = 2*DSIZE;
+    else
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+    /* 맞는 free 리스트 찾기 */
+    if((bp = find_fit(asize)) != NULL) {
+        place(bp, asize);
+        return bp;
     }
+    
+    /* 맞는게 없다면 메모리를 더가져와서 블록에 위치 시키기 */
+    extendsize = MAX(asize, CHUNKSIZE);
+    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+        return NULL;
+    place(bp, asize);
+    return bp;
+
+    // 원래 코드 
+    // int newsize = ALIGN(size + SIZE_T_SIZE);
+    // void *p = mem_sbrk(newsize);
+    // if (p == (void *)-1)
+	// return NULL;
+    // else {
+    //     *(size_t *)p = size;
+    //     return (void *)((char *)p + SIZE_T_SIZE);
+    // }
 }
 
 /*
@@ -135,6 +194,39 @@ void mm_free(void *ptr)
     PUT(FTRP(ptr), PACK(size, 0));
     coalesce(ptr);
 }
+
+static void *coalesce(void *bp) {
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size  = GET_SIZE(HDRP(bp));
+
+    if(prev_alloc && next_alloc) { // case 1
+        return bp;
+    }
+
+    else if (prev_alloc && !next_alloc) { // case 2
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+
+    else if (!prev_alloc && next_alloc) { // case3
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+
+    else {
+        size +=  GET_SIZE(HDRP(PREV_BLKP(bp))) +
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
+}
+
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
