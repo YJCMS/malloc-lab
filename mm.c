0f4f9b68608099da94f0d6290881c3b93128a2b3
YJCMS <sen9563@gmail.com>
2024-08-12 06:48:26 +0000

    constants and macros

--- mm.c
+++ mm.c
@@ -44,6 +44,31 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* Basic contstants and macro*/
+#define WSIZE 4 // Word and header, footer size(bytes)
+#define DSIZE 8 // Double word size(bytes)
+#define CHUNKSIZE (1<<12) // extend heap by this amount(bytes)
+#define MAX(x, y) ((x) > (y)? (x) : (y))
+
+/* Pack a siaze and allocated bit into a word */
+#define Pack(size, alloc) ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) == (val))
+
+/* Read the size and allocated field from address p */
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_SIZE(p) (GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given Block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char*)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char*)(bp) - DSIZE)))
+
 /* 
  * mm_init - initialize the malloc package.
  */
