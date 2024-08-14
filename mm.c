61872100483b46bff9561ccc3f0d698a459ece50
YJCMS <sen9563@gmail.com>
2024-08-14 11:37:50 +0000

    nextfit and test(81/100)

--- mm.c
+++ mm.c
@@ -36,40 +36,41 @@ team_t team = {
 };
 
 /* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+#define ALIGNMENT  // 메모리 정렬 단위
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7) // 주어진 alignment로 올림 처리
 
 
-#define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+#define SIZE_T_SIZE (ALIGN(sizeof(size_t))) // size_t 타입의 크기로 alignment로 정렬한 값을 저장
 
 /* Basic contstants and macro*/
-#define WSIZE 4 // Word and header, footer size(bytes)
-#define DSIZE 8 // Double word size(bytes)
-#define CHUNKSIZE (1<<12) // extend heap by this amount(bytes)
+#define WSIZE 4 // Word(4바이트) header, footer size
+#define DSIZE 8 // Double word size(8바이트)
+#define CHUNKSIZE (1<<12) // 힙을 확장 때 사용하는 기본 크기(4KB)
 #define MAX(x, y) ((x) > (y)? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
-#define PACK(size, alloc) ((size) | (alloc))
+#define PACK(size, alloc) ((size) | (alloc)) // 블록의 크기와 할당상태를 하나의 워드로 결합
 
 /* Read and write a word at address p */
-#define GET(p) (*(unsigned int *)(p))
-#define PUT(p, val) (*(unsigned int *)(p) = (val))
+#define GET(p) (*(unsigned int *)(p)) // 주어진 주소 p에서 4바이트 값을 읽어옴
+#define PUT(p, val) (*(unsigned int *)(p) = (val)) // 주어진 주소 p에 값을 기록
 
 /* Read the size and allocated field from address p */
-#define GET_SIZE(p) (GET(p) & ~0x7)
-#define GET_ALLOC(p) (GET(p) & 0x1)
+#define GET_SIZE(p) (GET(p) & ~0x7) // 할당 상태 비트를 제거하고 순수한 크기만
+#define GET_ALLOC(p) (GET(p) & 0x1) // 할당 상태만 가져옴
 
 /* Given block ptr bp, compute address of its header and footer */
-#define HDRP(bp) ((char *)(bp) - WSIZE)
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define HDRP(bp) ((char *)(bp) - WSIZE) // 주어진 블록 포인터 bp에서 블록 헤더 주소를 계산
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) // 주어진 블록 포인터 bp에서 풋터 주소 계산
 
 /* Given Block ptr bp, compute address of next and previous blocks */
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char*)(bp) - WSIZE)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char*)(bp) - DSIZE)))
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char*)(bp) - WSIZE))) // 다음 블록의 시작 주소를 계산
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char*)(bp) - DSIZE))) // 이전 블록의 시작 주소를 계산
 
 static char *heap_listp;
+static void *nextfit_lastp; // nextfit 적용을 위해 추가
 static void *extend_heap(size_t words);
 static void *coalesce(void *bp);
 
@@ -85,6 +86,7 @@ int mm_init(void)
     PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
     PUT(heap_listp + (3*WSIZE), PACK(0, 1));
     heap_listp += (2*WSIZE);
+    nextfit_lastp = heap_listp; // nextfit 적용을 위해 추가
 
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL) 
         return -1;
@@ -105,16 +107,22 @@ static void *extend_heap(size_t words) {
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
     return coalesce(bp);
-
 }
 
-/* first fit 적용 */
+/* next fit 적용 */
 static void *find_fit(size_t asize) {
-    /* first fit 탐색 */
+    /* next fit 탐색 */
     void *bp;
     
-    for(bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-        if (!GET_ALLOC(HDRP(bp))&& (asize <= GET_SIZE(HDRP(bp)))) {
+    // nextfit_lastp를 반복문 시작으로 설정(firstfit은 heap_listp기준으로 반복문하면 끝)
+    for (bp = nextfit_lastp; GET_SIZE(HDRP(bp)); bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+            return bp;
+        }
+    }
+    // 위 반복문에서 리턴되지 않은 경우 다시 처음부터 nextfit_lastp전까지 탐색 
+    for (bp = heap_listp; bp != nextfit_lastp; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
             return bp;
         }
     }
@@ -190,41 +198,46 @@ void mm_free(void *ptr)
 {
     size_t size = GET_SIZE(HDRP(ptr));
 
-    PUT(HDRP(ptr), PACK(size, 0));
-    PUT(FTRP(ptr), PACK(size, 0));
-    coalesce(ptr);
+    PUT(HDRP(ptr), PACK(size, 0)); // 헤더 0으로 표시
+    PUT(FTRP(ptr), PACK(size, 0)); // 푸터 0으로 표시
+    coalesce(ptr); // 비어있는 메모리 통합 
 }
 
 static void *coalesce(void *bp) {
-    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
-    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
-    size_t size  = GET_SIZE(HDRP(bp));
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));  // 이전 블록의 할당 상태
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));  // 다음 블록의 할당 상태
+    size_t size  = GET_SIZE(HDRP(bp));  // 현재 블록의 크기
 
-    if(prev_alloc && next_alloc) { // case 1
-        return bp;
+    if (prev_alloc && next_alloc) {  // 경우 1: 이전 블록과 다음 블록이 모두 할당됨
+        nextfit_lastp = bp; // nextfit 적용하기 위해서 리턴전에 현재 bp를 담아줌
+        return bp;  // 현재 블록만 반환 (병합할 블록이 없음)
     }
 
-    else if (prev_alloc && !next_alloc) { // case 2
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp), PACK(size, 0));
-        PUT(FTRP(bp), PACK(size, 0));
+    else if (prev_alloc && !next_alloc) {  // 경우 2: 이전 블록은 할당됨, 다음 블록은 비어있음
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));  // 현재 블록과 다음 블록 통합
+        PUT(HDRP(bp), PACK(size, 0));  // 현재 블록의 헤더 업데이트
+        PUT(FTRP(bp), PACK(size, 0));  // 현재 블록의 푸터 업데이트
     }
 
-    else if (!prev_alloc && next_alloc) { // case3
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        PUT(FTRP(bp), PACK(size, 0));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
+    else if (!prev_alloc && next_alloc) {  // 경우 3: 이전 블록은 비어있고, 다음 블록은 할당됨
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));  // 현재 블록과 이전 블록 통합
+        PUT(FTRP(bp), PACK(size, 0));  // 현재 블록의 푸터 업데이트
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));  // 이전 블록의 헤더 업데이트
+        bp = PREV_BLKP(bp);  // 포인터를 이전 블록으로 이동
     }
 
-    else {
-        size +=  GET_SIZE(HDRP(PREV_BLKP(bp))) +
-            GET_SIZE(FTRP(NEXT_BLKP(bp)));
-        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
-        bp = PREV_BLKP(bp);
+    else {  // 경우 4: 이전 블록과 다음 블록 모두 비어있음
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) +
+                GET_SIZE(FTRP(NEXT_BLKP(bp)));  // 현재 블록, 이전 블록, 다음 블록을 통합
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));  // 이전 블록의 헤더 업데이트
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));  // 다음 블록의 푸터 업데이트
+        bp = PREV_BLKP(bp);  // 포인터를 병합된 블록의 시작으로 이동
     }
-    return bp;
+
+    nextfit_lastp = bp; // nextfit 적용하기 위해서 리턴전에 현재 bp를 담아줌
+    return bp;  // 병합된 블록의 포인터를 반환\
+
+    
 }
 
 
